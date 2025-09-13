f118f1bc726cae7a65fbf25b5cef64365980791d
Subham <subham.mcabhu24@gmail.com>
2025-09-13 15:38:26 +0530

    Here we got some post req

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 // Send HTML file on GET request
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>Hello this is about page</h1>');
+});
+
+app.post('/about',(req,res)=>{
+    const username=req.body.username;
+    res.send(`<h1>Welcome ${username}</h1>`) ;
 });
 
 app.listen(PORT, () => {
